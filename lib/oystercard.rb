d0cc6f0a971f5bcdda0b2ce960048fa3a52f5e28
James Buckle <j.buckle93@hotmail.com>
2022-01-26 13:53:01 +0000

    oystercard changes

--- lib/oystercard.rb
+++ lib/oystercard.rb
@@ -1,20 +1,40 @@
 class Oystercard
+  attr_reader :balance, :in_journey, :MIN_FARE, :entry_station, :exit_station
 
-  MAXIMUM_BALANCE = 90
+  MAX_BALANCE = 90
+  MIN_BALANCE = 1
+  MIN_FARE = 1
   
-  attr_reader :balance
-
-  def initialize
-    @balance = 0
+  def initialize(balance = 0)
+    @balance = balance
+    # @in_journey = false
   end
 
   def top_up(amount)
-    fail 'Maximum balance exceeded' if amount + balance > MAXIMUM_BALANCE
+    fail "Max balance exceeded" unless @balance + amount <= MAX_BALANCE
     @balance += amount
   end
+
+  def in_journey?
+    !!entry_station
+    # @in_journey
+  end
+
+  def touch_in(station)
+    fail "insufficient balance" if balance < MIN_BALANCE
+    @entry_station = station
+    # @in_journey = true 
+  end
+
+  def touch_out(station)
+    deduct(MIN_FARE)
+    @exit_station = station
+    @entry_station = nil
+  end
+
+  private
 
   def deduct(amount)
     @balance -= amount
   end
-
 end
--- spec/oystercard_spec.rb
+++ spec/oystercard_spec.rb
@@ -1,27 +1,83 @@
 require 'oystercard'
 
-describe Oystercard do 
-  it 'has a balance of zero' do
-    expect(subject.balance).to eq (0)
+describe Oystercard do
+
+  let(:oystercard) {Oystercard.new}
+  let(:entry_station){ double :station }
+  let(:exit_station){ double :station }
+  
+  it {expect(oystercard).to respond_to(:balance)}
+
+  it "checks that the default balance is 0" do
+    expect(oystercard.balance).to eq(0)
+  end
+  
+  it {expect(oystercard).to respond_to(:top_up)}
+
+  it "checks that top_up will add a given amount to the balance" do
+    oystercard.top_up(50)
+    expect(oystercard.balance).to eq(50)
+  end
+  
+  it "checks that top_up will sum given amounts to the balance" do
+    oystercard.top_up(10)
+    oystercard.top_up(10)
+    expect(oystercard.balance).to eq(20)
   end
 
-  describe '#top_up' do
+  it {expect(oystercard).to respond_to(:top_up)}
 
-    it { is_expected.to respond_to(:top_up).with(1).argument }
-
-    it 'can top up the balance' do
-      expect{ subject.top_up 1 }.to change{ subject.balance }.by 1
-    end
-
-    it 'raises an error if the maximum balance is exceeded' do
-      maximum_balance = Oystercard::MAXIMUM_BALANCE 
-      subject.top_up maximum_balance
-      expect{ subject.top_up 1 }.to raise_error "Maximum balance exceeded"
-    end
-
-    it 'deducts an amountfrom the balance' do
-      subject.top_up(20)
-      expect{ subject.deduct 3}.to change{ subject.balance }.by -3
-    end
+  it "fails if the top_up increases balance beyond the limit" do
+    expect {oystercard.top_up(1999)}.to raise_error 'Max balance exceeded'
   end
-end 
+
+  it "should deduct a fare from the card" do
+    oystercard.top_up(50)
+    oystercard.touch_out(exit_station)
+    expect(oystercard.balance).to eq(49)
+  end
+
+  it "is initially not in journey" do
+    expect(oystercard).not_to be_in_journey
+  end
+
+  it "can touch in" do
+    oystercard.top_up(5)
+    oystercard.touch_in(entry_station)
+    expect(oystercard).to be_in_journey
+  end
+
+  it "can touch out" do
+    oystercard.touch_out(exit_station)
+    expect(oystercard).not_to be_in_journey
+    expect { oystercard.touch_out(exit_station) }.to change { oystercard.balance }.by (-Oystercard::MIN_FARE)
+  end
+
+  it 'will not touch in if under minimum balance' do
+    expect{ oystercard.touch_in(entry_station) }.to raise_error "insufficient balance"
+  end
+
+  # it 'deducts a minimum fare from card when used' do
+  #   expect { oystercard.touch_out }.to change { oystercard.balance }.by (-Oystercard::MIN_FARE)
+  # end
+
+  it "stores the entry station" do
+    oystercard.top_up(40)
+    oystercard.touch_in(entry_station)
+    expect(oystercard.entry_station).to eq entry_station
+  end
+
+  it "removes entry station after touch out" do
+    oystercard.top_up(40)
+    oystercard.touch_in(entry_station)
+    oystercard.touch_out(exit_station)
+    expect(oystercard).not_to be_in_journey
+  end
+
+  it "stores exit station" do
+    oystercard.top_up(40)
+    oystercard.touch_in(entry_station)
+    oystercard.touch_out(exit_station)
+    expect(oystercard.exit_station).to eq exit_station
+  end
+end
